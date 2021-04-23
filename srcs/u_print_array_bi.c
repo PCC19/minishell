c8b70f40c4b8cfac81f8c2f5a601deb4f5986930
Paulo Cunha <pcunha00@hotmail.com>
2021-04-23 19:10:46 +0200

    pipes ok, u_print_array_bi ok, ft_split3 ok

--- srcs/u_print_array_bi.c
+++ srcs/u_print_array_bi.c
@@ -6,7 +6,7 @@
 /*   By: user42 <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/08 22:11:16 by user42            #+#    #+#             */
-/*   Updated: 2021/04/23 18:27:17 by user42           ###   ########.fr       */
+/*   Updated: 2021/04/23 19:07:14 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,13 +20,19 @@ void	u_print_array_bi(t_v *v, char **s)
 
 	(void) v;
 
+	if (fcntl(v->cmd.save_out, F_GETFD) != -1)
+		aux = v->cmd.save_out;
+	else
+		aux = 1;
+
 
 	if (s!= NULL)
 	{
 		i = 0;
 		while (s[i] != 0)
 		{
-			dprintf(v->cmd.save_out,"|%s|\n", (s[i]));
+			dprintf(aux,"|%s|\n", (s[i]));
+			//dprintf(v->cmd.save_out,"|%s|\n", (s[i]));
 			//dprintf(1,"|%s|\n", (s[i]));
 			i++;
 		}
