50865f2c9ac50a39da203866dedf1e1a27a2fd7a
Paulo Cunha <pcunha00@hotmail.com>
2021-04-28 02:57:33 +0200

    pipelines and redirects ok

--- srcs/parse_in_red.c
+++ srcs/parse_in_red.c
@@ -6,7 +6,7 @@
 /*   By: user42 <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/09 23:58:59 by user42            #+#    #+#             */
-/*   Updated: 2021/04/22 02:26:20 by user42           ###   ########.fr       */
+/*   Updated: 2021/04/28 01:03:15 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,8 @@ void	parse_in_red(t_v *v, int *k, int in)
 	copy_until(file_redir, v->expandido, " <>", k);
 	printf("file_redir: %s\n", file_redir);
 	v->cmd.fd_in_red = open(file_redir, O_RDONLY | O_CREAT);
+		dprintf(v->cmd.save_out, "%s %s\n", file_redir, strerror(errno));
+		errno = 0;
 	dup2(v->cmd.fd_in_red, in);
 	close(v->cmd.fd_in_red);
 
