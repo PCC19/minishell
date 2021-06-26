39119ac8a63b668fe59a1f9dc4d0b4fc2abc9f8f
Paulo Cunha <pcunha00@hotmail.com>
2021-06-27 00:22:37 +0200

    merge qith cezar2

--- srcs/execute_command.c
+++ srcs/execute_command.c
@@ -6,7 +6,7 @@
 /*   By: cpereira <cpereira@student.42sp.org>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/07 01:11:30 by user42            #+#    #+#             */
-/*   Updated: 2021/06/26 12:15:27 by cpereira         ###   ########.fr       */
+/*   Updated: 2021/06/27 00:21:04 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,6 +41,7 @@ void	execute_command(t_v *v)
 
 void	get_env(t_v *v)
 {
+	u_print_array_bi(v, v->env);
 	set_return_status(v, EXIT_SUCCESS);
 }
 
